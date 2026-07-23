c373e0dbc1d24b1ba692dcbc41e4d48c18a9502c
Sudip Chaudhary <chsudip30@gmail.com>
2026-07-23 09:13:36 +0545

    Udating the rootlayout

--- frontend/app/layout.js
+++ frontend/app/layout.js
@@ -6,12 +6,3 @@ export const metadata = {
   viewport: 'width=device-width, initial-scale=1, maximum-scale=1',
 };
 
-export default function RootLayout({ children }) {
-  return (
-    <html lang="en" suppressHydrationWarning>
-      <body suppressHydrationWarning className="min-h-screen overflow-x-hidden bg-[#F8FAFC] text-slate-800">
-        {children}
-      </body>
-    </html>
-  );
-}
